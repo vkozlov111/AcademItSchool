bb7ae535e4a6d4ce1e08ad14c955d27bc176547b
Вячеслав <vkozlov111@yandex.ru>
2018-09-27 12:12:43 +0700

    Первый commit

---
+++ Range/src/ru/academits/vkozlov111/Range/Main.java
@@ -0,0 +1,25 @@
+package ru.academits.vkozlov111.Range;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        System.out.println("Введите первое и последнее число интервала: ");
+        double startPoint = scanner.nextDouble();
+        double endPoint = scanner.nextDouble();
+        Range range = new Range(startPoint, endPoint);
+        System.out.println("Длина интервала равна " + range.getIntervalLength());
+
+        System.out.println("Введите число для проверки принадлежности интервалу:");
+        double isInsideNumber = scanner.nextDouble();
+        System.out.println("Число " + isInsideNumber + ((range.isInside(isInsideNumber) ? "" : " не") + " принадлежит интервалу"));
+
+    }
+}
---
+++ Range/src/ru/academits/vkozlov111/Range/Range.java
@@ -0,0 +1,21 @@
+package ru.academits.vkozlov111.Range;
+
+class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getIntervalLength() {
+        return this.to - this.from;
+    }
+
+    public boolean isInside(double isInsideNumber) {
+        return ((isInsideNumber >= this.from) && (isInsideNumber <= this.to));
+    }
+
+}
+
