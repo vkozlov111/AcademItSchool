3d21eecf68250b273964f6dcadfdd74558e09187
Вячеслав <vkozlov111@yandex.ru>
2018-10-01 15:54:25 +0700

    Range с пересечением 1вар

--- Range/src/ru/academits/vkozlov111/Range/Main.java
+++ Range/src/ru/academits/vkozlov111/Range/Main.java
@@ -6,19 +6,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
-
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
-
+/*
         System.out.println("Введите первое и последнее число интервала: ");
         double startPoint = scanner.nextDouble();
         double endPoint = scanner.nextDouble();
-        Range range = new Range(startPoint, endPoint);
-        System.out.println("Длина интервала равна " + range.getIntervalLength());
+        Range firstRange = new Range(startPoint, endPoint);
+        System.out.println("Длина интервала равна " + firstRange.getIntervalLength());
 
         System.out.println("Введите число для проверки принадлежности интервалу:");
         double isInsideNumber = scanner.nextDouble();
-        System.out.println("Число " + isInsideNumber + ((range.isInside(isInsideNumber) ? "" : " не") + " принадлежит интервалу"));
+        System.out.println("Число " + isInsideNumber + ((firstRange.isInside(isInsideNumber) ? "" : " не") + " принадлежит интервалу"));
+*/
+        double startPoint = 0;
+        double endPoint = 10;
+        Range firstRange = new Range(startPoint, endPoint);
+
+        System.out.println("Введите первое и последнее число второго интервала: ");
+        double secondStartPoint = scanner.nextDouble();
+        double secondEndPoint = scanner.nextDouble();
+
+        Range secondRange = new Range(secondStartPoint, secondEndPoint);
+        Range intersectionOfRanges = firstRange.getIntersection(secondRange);
+
+        if (intersectionOfRanges != null) {
+            System.out.println("Начало и конец пересечения: " + intersectionOfRanges.getFrom() + " " + intersectionOfRanges.getTo());
+        } else System.out.println("Для данных интервалов пересечения не существует");
+
     }
 }
--- Range/src/ru/academits/vkozlov111/Range/Range.java
+++ Range/src/ru/academits/vkozlov111/Range/Range.java
@@ -4,9 +4,21 @@ class Range {
     private double from;
     private double to;
 
-    public Range(double from, double to) {
-        this.from = from;
-        this.to = to;
+    Range(double from, double to) {
+        if (from > to) {
+            System.out.println("'from' не может быть меньше 'to'");
+        } else {
+            this.from = from;
+            this.to = to;
+        }
+    }
+
+    double getFrom() {
+        return this.from;
+    }
+
+    double getTo() {
+        return this.to;
     }
 
     public double getIntervalLength() {
@@ -15,5 +27,26 @@ class Range {
 
     public boolean isInside(double isInsideNumber) {
         return ((isInsideNumber >= this.from) && (isInsideNumber <= this.to));
+    }
+
+    Range getIntersection(Range inputRange) {
+        Range intersectionOfRanges = new Range(from, to);
+
+        if ((this.from > inputRange.to) || (this.to < inputRange.from)) {
+            return null;
+        } else {
+            if (this.from < inputRange.from) {
+                intersectionOfRanges.from = inputRange.from;
+            } else {
+                intersectionOfRanges.from = this.from;
+            }
+
+            if (this.to > inputRange.to) {
+                intersectionOfRanges.to = inputRange.to;
+            } else {
+                intersectionOfRanges.to = this.to;
+            }
+        }
+        return intersectionOfRanges;
     }
 }
